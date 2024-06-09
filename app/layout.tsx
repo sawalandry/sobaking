1a5108cf5cdafb37f539e2caa8134d8e025e926c
INGABIRE Landry <machinelab2021@gmail.com>
2024-06-09 13:29:23 +0200

    my basic file

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font--inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif'
+})
+
+export const metadata: Metadata = {
+  title: "Sawa Online Bank",
+  description: "Sawa Online Bank is a Modern Banking Platform for everyone",
+  icons: 'icons/logo.svg'
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+        <div>
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
